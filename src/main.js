1494f31fa1582b9e39edfffae29d849b6e45cf55
Mr.Peng <971107503@qq.com>
2017-12-05 09:54:00 +0800

    配置综合, 基础css引入

--- src/main.js
+++ src/main.js
@@ -2,14 +2,36 @@ import Vue from 'vue';
 //引入vue
 import App from './App.vue';
 //引入 注主模块
-import vueRouter  from 'vue-router';
-//引入路由
-Vue.use(vueRouter)
+
 //注入分模块
+
+
+// 4.0 注册mint-ui
+// 导入mint-ui的css文件
+import 'mint-ui/lib/style.min.css';
+// 导入mint-ui组件对象
+import mintui from 'mint-ui';
+// 在Vue中全局使用mintui
+Vue.use(mintui);
+
+// 5.0 注册mui的css样式
+import '../statics/mui/css/mui.css';
+
+// 6.0 导入一个当前系统的全局基本样式
+import '../statics/css/site.css';
+
+// 7.0 将vue-resource在vue中绑定，自动在vue实例对象上注入一个$http对象就可以使用ajax方法请求数据
+
+import vueRouter  from 'vue-router';
+Vue.use(vueRouter)
+
 import login  from './components/account/login.vue';
 import register  from './components/account/register.vue';
 
+
+
 var router = new vueRouter({
+    linkActiveClass:'miu-active',
     routes:[
         {
             path:"/login",
